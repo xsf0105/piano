adc1deb3fc0b3401e86147871cc7a2ca006b1906
xsf0105 <allan91coder@gmail.com>
2023-10-25 14:09:49 +0800

    update

--- vite.config.build.ts
+++ vite.config.build.ts
@@ -1,33 +1,3 @@
-// import { resolve } from "path";
-// import { defineConfig } from "vite";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   build: {
-//     lib: {
-//       entry: resolve("./src/index.tsx"),
-//       formats: ["es", "umd"], // 打包输出格式，默认输出 esm/umd
-//       fileName: (format, entryName) => {
-//         if (format === "es") {
-//           return `${entryName}.js`;
-//         }
-
-//         return `${entryName}.${format}.js`;
-//       },
-//       name: "MyComponent",
-//     },
-//     rollupOptions: {
-//       external: ["quarkc"], // 可选项，是否将 quarkc 打包进组件
-//       output: {
-//         dir: "lib",
-//         globals: {
-//           quarkc: "Quarkc",
-//         },
-//       },
-//     },
-//   },
-// });
-
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import copyPlugin from 'rollup-plugin-copy'
@@ -35,15 +5,18 @@ import copyPlugin from 'rollup-plugin-copy'
 export default defineConfig({
   base: './',
   build: {
+    emptyOutDir: false, // 复制文件夹，需要将此配置项设为 false
     rollupOptions: {
       plugins: [
         copyPlugin({
-          targets: [{ src: 'samples', dest: 'dist' }], // 将文件复制到 dist/healthcheck, vite会自动复制到dist目录
+          targets: [
+            { src: 'samples', dest: 'dist' }
+          ],
         }),
       ],
       input: {
         main: resolve(__dirname, 'index.html'),
-      },
+      }
     },
   },
 })
